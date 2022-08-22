e983fd70a6023b2e588a1b6d27830425cf8293a8
ChongLeangUENG <leangcnrp777@gmail.com>
2022-08-22 04:20:17 -0700

    Commit function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,10 +24,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	/**
-	 * if (size < 2)
+	if (*array == NULL || size == 0)
 		return;
-		*/
+
+	/* if (size < 2)
+		return; */
 
 	for (i = 0; i < size - 1; i++)
 	{
