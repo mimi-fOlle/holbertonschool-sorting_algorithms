ecca45d83b3d03d2d21f415b090611537a9722e0
ChongLeangUENG <leangcnrp777@gmail.com>
2022-08-22 04:10:40 -0700

    Commit function that sorts an array of integers in ascending order using the Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * swap - function that swap two elements
+ * @i: First number
+ * @j: Second number
+ */
+void swap(int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+/**
+ * bubble_sort - unction that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: Array to sort
+ * @size: Size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j+ 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
